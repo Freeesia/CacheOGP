4fe7f72d9d5166f29271415c2ef6980f2a999ad2
Freesia <freesia@studiofreesia.com>
2024-08-03 03:09:44 +0900

    Docker検証用の設定追加

--- CacheOGP.AppHost/Program.cs
+++ CacheOGP.AppHost/Program.cs
@@ -5,6 +5,8 @@ var data = builder.AddPostgres("data");
 var ogp = data.AddDatabase("ogp");
 
 builder.AddProject<Projects.CacheOGP_ApiService>("cache-ogp")
+// builder.AddDockerfile("cache-ogp", "..", "Dockerfile")
+//     .WithHttpEndpoint(targetPort: 8080)
     .WithReference(cache)
     .WithReference(ogp);
 
